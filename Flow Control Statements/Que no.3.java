bdd72abe65b9c8caf5c15c2840af3e7f73e605e1
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-05-29 16:42:33 +0530

    Update Que no.3.java

--- Flow Control Statements/Que no.3.java
+++ Flow Control Statements/Que no.3.java
@@ -1,1 +1,28 @@
+class Simple
+{
+public static void main(String [] args)
+{
+if(args.length==0)
+ System.out.println("No Values");
+else
+{
+ for(int i = 0;i<args.length;i++)
+ {
+    System.out.print(args[i]);
+    if(i<args.length-1)
+    {
+        System.out.print(",");
+    }
+  }    
+}
+}
+}
 
+OUTPUT:
+
+C:\Users\Mageshwar\Desktop>java Simple
+No Values
+
+C:\Users\Mageshwar\Desktop>java Simple mumbai banglore
+mumbai,banglore
+C:\Users\Mageshwar\Desktop>
