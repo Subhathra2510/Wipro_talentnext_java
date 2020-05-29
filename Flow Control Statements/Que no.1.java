c118e0bae740cfdbfc59d00bb0f2b248dfb8f093
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-05-29 11:27:03 +0530

    Update Que no.1.java

--- Flow Control Statements/Que no.1.java
+++ Flow Control Statements/Que no.1.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+
+public class SignOfTheInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		if(a>0)
+			System.out.println("The given number is a positive number");
+		else if(a<0)
+			System.out.println("The given number is a negative number");
+		else
+			System.out.println("The given number is zero");
+	}
+
+}
+
+OUTPUT:
+-67
+The given number is a negative number
+43
+The given number is a positive number
+0
+The given number is zero
 
