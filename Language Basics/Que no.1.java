2b47616b8c7ca24f0a0914252eebb6ecbb4a8bef
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-05-29 10:51:11 +0530

    Update Que no.1.java

--- Language Basics/Que no.1.java
+++ Language Basics/Que no.1.java
@@ -1,1 +1,17 @@
+//que no. 1
 
+class CommandLineArguement
+{
+public static void main(String [] args)
+{
+ System.out.println(args[0]+" Technologies "+args[1]);
+}
+}
+
+
+OUTPUT:
+
+C:\Users\Mageshwar\Desktop>javac CommandLineArguement.java
+
+C:\Users\Mageshwar\Desktop>java CommandLineArguement Wipro Banglore
+Wipro Technologies Banglore
