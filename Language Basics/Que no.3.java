3c1f8bef666e6449d2e4759f5d4e9b47cfb1a6e2
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-05-29 10:54:47 +0530

    Update Que no.3.java

--- Language Basics/Que no.3.java
+++ Language Basics/Que no.3.java
@@ -1,1 +1,17 @@
+//Que no. 3
 
+class Integers
+{
+public static void main(String [] args)
+{
+int a = Integer.parseInt(args[0])+ Integer.parseInt(args[1]);
+System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+a);
+}
+}
+
+OUTPUT:
+
+C:\Users\Mageshwar\Desktop>javac Integers.java
+
+C:\Users\Mageshwar\Desktop>java Integers 10 20
+The sum of 10 and 20 is 30
