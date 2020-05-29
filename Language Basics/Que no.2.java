56284f3bac12276bf394d370d69f84993ec6a4d1
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-05-29 10:53:11 +0530

    Update Que no.2.java

--- Language Basics/Que no.2.java
+++ Language Basics/Que no.2.java
@@ -1,1 +1,17 @@
+//Que no.2
 
+class Sample
+{
+public static void main(String [] args)
+{
+System.out.println("Welcome "+args[0]);
+}
+}
+
+
+OUTPUT:
+
+C:\Users\Mageshwar\Desktop>javac Sample.java
+
+C:\Users\Mageshwar\Desktop>java Sample john
+Welcome john
