bd1d427ff58334ed1211ae1df1b158f186a4b48b
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-05-29 11:33:44 +0530

    Update Que no.2.java

--- Flow Control Statements/Que no.2.java
+++ Flow Control Statements/Que no.2.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+
+public class SignOfTheInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		if(a<0)
+		{
+			a=a*-1;
+		}
+		if(a%2==0)
+			System.out.println("The given number is a even number");
+		else
+			System.out.println("The given number is a odd number");
+	}
+
+}
+
+
+OUTPUT:
+67
+The given number is a odd number
+34
+The given number is a even number
 
