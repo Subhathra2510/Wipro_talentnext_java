e35be2c2906f3c15e42836da5ad0aaa121627c6f
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-06-08 01:15:22 +0530

    Update Of_given_5_numbers_how_many_are_odd?.java

--- Day-5/Of_given_5_numbers_how_many_are_odd?/Of_given_5_numbers_how_many_are_odd?.java
+++ Day-5/Of_given_5_numbers_how_many_are_odd?/Of_given_5_numbers_how_many_are_odd?.java
@@ -1,1 +1,24 @@
-
+int count = 0;
+		if(input1<0) input1=input1*-1;
+		if(input2<0) input2=input2*-1;
+		if(input3<0) input3=input3*-1;
+		if(input4<0) input4=input4*-1;
+		if(input5<0) input5=input5*-1;
+		  if(input1==0) return 1;
+      if(input1%2==1)
+		    count++;
+      if(input2==0) return 1;
+		  if(input2%2==1)
+		    count++;
+      if(input3==0) return 1;
+		  if(input3%2==1)
+		    count++;
+      if(input4==0) return 1;
+		  if(input4%2==1)
+		    count++;
+      if(input5==0) return 1;
+		  if(input5%2==1)
+		    count++;
+		  
+		
+return count;
