447a084b49bb51f5d2e17f81c0b36aff7e879f67
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-06-08 00:58:43 +0530

    Update Of_5_numbers,_how_many_are_even_or_odd?.java

--- Day-5/Of_5_numbers,_how_many_are_even_or_odd?/Of_5_numbers,_how_many_are_even_or_odd?.java
+++ Day-5/Of_5_numbers,_how_many_are_even_or_odd?/Of_5_numbers,_how_many_are_even_or_odd?.java
@@ -1,1 +1,35 @@
-
+int count = 0;
+		if(input1<0) input1=input1*-1;
+		if(input2<0) input2=input2*-1;
+		if(input3<0) input3=input3*-1;
+		if(input4<0) input4=input4*-1;
+		if(input5<0) input5=input5*-1;
+		if(input6.equals("even"))
+		{
+          if(input1==0 || input1%2==0)
+		    count++;
+		  if(input2==0 || input2%2==0)
+		    count++;
+		  if(input3==0 || input3%2==0)
+		    count++;
+		  if(input4==0 || input4%2==0)
+		    count++;
+		  if(input5==0 || input5%2==0)
+		    count++;
+		  
+		}
+		else if(input6.equals("odd"))
+		{
+          if(input1%2==1)
+		    count++;
+		  if(input2%2==1)
+		    count++;
+		  if(input3%2==1)
+		    count++;
+		  if(input4%2==1)
+		    count++;
+		  if(input5%2==1)
+		    count++;
+		  
+		}
+return count;
