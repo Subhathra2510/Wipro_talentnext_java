9a527b7da1b849b631f07ae77b4540f5aa82ff73
Subhathra2510 <64795645+Subhathra2510@users.noreply.github.com>
2020-06-08 00:36:47 +0530

    Update Is_N_an_exact_multiple_of_M?.java

--- Day-5/Is_N_an_exact_multiple_of_M?/Is_N_an_exact_multiple_of_M?.java
+++ Day-5/Is_N_an_exact_multiple_of_M?/Is_N_an_exact_multiple_of_M?.java
@@ -1,1 +1,14 @@
-
+ if(input1<0)
+		{
+			input1=input1*-1;
+		}
+	                if(input1==0 || input2==0)
+		{
+			return 3;
+		}
+		else if(input1%input2==0)
+		{
+			return 2;
+		}
+		else
+		   return 1;
